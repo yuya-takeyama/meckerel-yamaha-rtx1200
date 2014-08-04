d492d010f0f6495d3ee3c8f15821fe9e54e12e13
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-08-04 09:12:16 +0000

    Initial commit

---
+++ lib/meckerel/yamaha/rtx1200.rb
@@ -0,0 +1,9 @@
+require "meckerel/yamaha/rtx1200/version"
+
+module Meckerel
+  module Yamaha
+    module Rtx1200
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/meckerel/yamaha/rtx1200/version.rb
@@ -0,0 +1,7 @@
+module Meckerel
+  module Yamaha
+    module Rtx1200
+      VERSION = "0.0.1"
+    end
+  end
+end
